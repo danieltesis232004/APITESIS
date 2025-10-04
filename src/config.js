c32d5db805e61fef975ab5c39715a1a90280e644
danieltesis232004 <danieltesis232004@gmail.com>
2025-10-04 17:56:06 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ console.log('Variables de entorno:', {
 
 export const BD_HOST = process.env.BD_HOST || 'mysql-danieltesis.alwaysdata.net'
 export const BD_DATABASE = process.env.BD_DATABASE || 'danieltesis_APP'
-export const BD_USER = process.env.BD_USER || '433568_daniel'
+export const BD_USER = process.env.BD_USER || '433568'
 export const BD_PASSWORD = process.env.BD_PASSWORD || 'D23T07S2004'
 export const BD_PORT = process.env.BD_PORT || 3306
 export const PORT = process.env.PORT || 10000
