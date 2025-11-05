10e59698f4011d7efd920f5f94414b435da418f7
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-05 01:47:54 -0500

    Agrego función obtenerUltimoCodigo y actualizo rutas de recuperación

--- src/config.js
+++ src/config.js
@@ -1,14 +1,5 @@
 import {config} from 'dotenv'
 config()
-
-console.log('Variables de entorno:', {
-  BD_HOST: process.env.BD_HOST,
-  BD_DATABASE: process.env.BD_DATABASE,
-  BD_USER: process.env.BD_USER,
-  BD_PASSWORD: process.env.BD_PASSWORD,
-  BD_PORT: process.env.BD_PORT
-});
-
 export const BD_HOST = process.env.BD_HOST || 'mysql-danieltesis.alwaysdata.net'
 export const BD_DATABASE = process.env.BD_DATABASE || 'danieltesis_app'
 export const BD_USER = process.env.BD_USER || '433568'
