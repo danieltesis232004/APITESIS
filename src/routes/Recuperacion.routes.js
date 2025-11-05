eb858899ac254491ad03a8a8432cc816ea41afc7
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-05 01:42:38 -0500

    Agrego función obtenerUltimoCodigo y actualizo rutas de recuperación

--- src/routes/Recuperacion.routes.js
+++ src/routes/Recuperacion.routes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import {
   crearRecuperacion,
   verificarCodigo,
-  almacenarcodigoRecuperacion
+  obtenerUltimoCodigo
 } from '../controladores/RecuperacionCtrl.js';
 
 const router = express.Router();
 
 router.post('/recuperacion/:id_usuario', crearRecuperacion);
 router.get('/recuperacion/verificar/:id_usuario/:codigo', verificarCodigo);
+router.get('/recuperacion/ultimo/:id_usuario', obtenerUltimoCodigo);
+
 export default router;
