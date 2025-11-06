ee9e387b667ba2496d2a4fc60d23209f1d04ac4a
danieltesis232004 <danieltesis232004@gmail.com>
2025-11-06 17:51:48 -0500

    Actualizo controlador con Resend

--- src/controladores/emailCtrl.js
+++ src/controladores/emailCtrl.js
@@ -8,7 +8,7 @@ export const enviarCodigoPorCorreo = async (req, res) => {
     apiInstance.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
 
     const sendSmtpEmail = {
-      sender: { name: 'Soporte Inédita', email: '9afdde001@smtp-brevo.com' },
+      sender: { name: 'Soporte Inédita', email: 'danieltesis232004@gmail.com' },
       to: [{ email: correoDestino }],
       subject: 'Código de recuperación',
       htmlContent: `
